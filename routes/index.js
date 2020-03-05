56c51be194392c92671edf717694fe6af4de4ac8
oscarliusy <1069176850@qq.com>
2020-03-05 21:41:29 +0800

    socket

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  // res.render('index', { title: 'Express222' });
+  res.sendFile(__dirname + '/index.html') //__dirname = 当前绝对路径
 });
 
 module.exports = router;
